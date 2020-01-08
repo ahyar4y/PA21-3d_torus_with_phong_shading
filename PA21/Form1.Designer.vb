b37a511227595e40763e8a3298f3f71fe5a10490
guricchi08 <ahyar4y08@gmail.com>
2020-01-08 21:24:41 +0700

    Finished. TODO: interpolate z and surface detection

--- PA21/Form1.Designer.vb
+++ PA21/Form1.Designer.vb
@@ -561,7 +561,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
-        Me.Text = "pma"
+        Me.Text = "Phong Shading on Torus"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NumericUpDown5, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.NumericUpDown6, System.ComponentModel.ISupportInitialize).EndInit()
